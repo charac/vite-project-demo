23ab077aedf442ee1b27b3580ea076b3de9f8d5a
236604099@qq.com <236604099@qq.com>
2022-05-26 16:46:20 +0800

    配置pnpm dev

--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -1,13 +1,13 @@
 <!--
  * @since: 2022-05-18 09:36:34
  * @LastAuthor: Do not edit
- * @lastTime: 2022-05-24 09:11:51
+ * @lastTime: 2022-05-26 16:42:45
  * @Author: ltm@xtoneict.com
  * @message: 登录界面
 -->
 <template>
   <div>
-
+    胜多负少
   </div>
 </template>
 
