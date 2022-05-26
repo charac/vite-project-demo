714de9679c3cd71ed9fdfb7455b4357a7c79c498
236604099@qq.com <236604099@qq.com>
2022-05-26 17:08:39 +0800

    配置element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @since: 2022-05-13 15:51:54
- * @LastAuthor: Please set LastEditors
- * @lastTime: 2022-05-16 18:10:28
+ * @LastAuthor: Do not edit
+ * @lastTime: 2022-05-26 17:04:47
  * @Author: ltm@xtoneict.com
  * @message: 配置引入
  */
@@ -9,9 +9,10 @@ import { createApp } from 'vue';
 import router from './router';
 import store from './store';
 import App from './App.vue';
-// import { registerApp } from './global';
+import { registerApp } from './global';
 import 'normalize.css/normalize.css';
-
+import consola from 'consola';
+consola.log(registerApp);
 createApp(App)
     // .registerApp(app)
     .use(router)
